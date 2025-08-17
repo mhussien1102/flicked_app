6ff6897c97a153256dd5ca5fcd3a90477132f0e7
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-08-17 21:43:00 +0300

    create the Search bar of the App

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,18 +1,103 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MainPage extends ConsumerWidget {
   MainPage({super.key});
 
+  static double deviceWidth = 0;
+  static double deviceHeight = 0;
+
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return _buildUI();
+    deviceWidth = MediaQuery.of(context).size.width;
+    deviceHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: deviceWidth,
+        height: deviceHeight,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [buildBackground(), buildForeground()],
+        ),
+      ),
+    );
   }
 
-  Widget _buildUI() {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Main Page')),
-      body: const Center(child: Text('Hello from MainPage!')),
+  Widget buildBackground() {
+    return Container(
+      height: deviceHeight, // ✅ بقت متاحة هنا
+      width: deviceWidth,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: const DecorationImage(
+          image: NetworkImage(
+            'https://images-na.ssl-images-amazon.com/images/I/91B32iU7ayL._AC_SL1500_.jpg',
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
+        ),
+      ),
+    );
+  }
+
+  Widget buildForeground() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, deviceHeight * 0.02, 0, 0),
+      width: deviceHeight * 0.88,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [topBarWidget()],
+      ),
+    );
+  }
+
+  Widget topBarWidget() {
+    return Container(
+      height: deviceHeight * 0.08,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.black54,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [searchBarWidget()],
+      ),
+    );
+  }
+
+  Widget searchBarWidget() {
+    final _border = InputBorder.none;
+    return SizedBox(
+      height: deviceHeight * 0.05,
+      width: deviceWidth * 0.50,
+      child: TextField(
+        controller: searchController,
+        onSubmitted: (intput) {},
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          focusedBorder: _border,
+          border: _border,
+          prefixIcon: Icon(Icons.search, color: Colors.white24),
+          hintText: 'Search....',
+          hintStyle: TextStyle(color: Colors.white54),
+          filled: false,
+          fillColor: Colors.white24,
+        ),
+      ),
     );
   }
 }
